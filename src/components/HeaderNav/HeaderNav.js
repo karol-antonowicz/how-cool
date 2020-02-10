b4cdf0b123d6efef12a8206ac8ea5d26b1450a16
Karol Antonowicz <karol.antonowicz@gmail.com>
2020-02-10 04:10:32 +0100

    nav menu

--- src/components/HeaderNav/HeaderNav.js
+++ src/components/HeaderNav/HeaderNav.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './HeaderNav.scss';
 
 const infoShareWebSiteAdress = 'https://www.infoshare.pl';
@@ -7,142 +7,116 @@ const gdanskWebSiteAdress = 'https://www.gdansk.pl'
 export const HeaderNav = () => {
 
     const [menu, setMenu] = useState(false);
+    const [bigScreen, setBigScreen] = useState(false)
 
     const toggleMenu = () => {
         console.log('dupa')
         setMenu(!menu)
     }
 
+    const toggleBigScreen = () => {
+        if (window.innerWidth > 1200) {
+            setBigScreen(true)
+            console.log('chuj')
+        }
+    }
+
+    useEffect(()=> {
+        setInterval(toggleBigScreen, 20);
+    })
+
+    const infoShareArr = ['Stages', 'Side Events', 'Venue', 'Become a Volunteer', 'Media', 'Hotels', 'Discover Gdańsk', 'FAQ'];
+    const whyAttendArr = ['Developer / IT Specialist', 'Marketer', 'Executive / CxO', 'Startup', 'Investor / Business Angel', 'Tech Fan'];
+    const speakersArr = ['Infoshare 2020 Speakers', 'Call for Speakers'];
+    const startUpsArr = ['Startup Contest', 'Speed Dating', 'Why attend - Startup', 'Webinar'];
+    const ourProjectsArr = ['AI & Data Science Conference / Warsaw', 'Pipeline Summit', 'Future3', 'GrowTech Magazine', 'Poland Prize', 'Global CEO Research', 'AI Developers raport', 'Cybersec Guide'];
+    const aboutArr = ['Job', 'Blog', 'IS Team', 'Contact', 'RE:infoShare']
+
+    const infoShareLinks = infoShareArr.map(el => (
+        <li>
+            <a className='drop-text' alt={el} href={`https://infoshare.pl/${el.split(" ").join("-").toLowerCase()}`}>{el}</a>
+        </li>
+    ))
+
+    const whyAttendLinks = whyAttendArr.map(el => (
+        <li>
+            <a className='drop-text' alt={el} href={`https://infoshare.pl/${el.split(" ").join("-").toLowerCase()}`}>{el}</a>
+        </li>
+    ))
+
     return (
         <div className='header-nav'>
             <div className='wrapper'>
-            <div className='top-header'>
-                <div className='logo'>
-                    <a href={infoShareWebSiteAdress} className='infoshare-logo' alt='infoShare' />
-                    <a href={gdanskWebSiteAdress} className='gdansk-logo' alt='Gdansk city' />
-                </div>
-                <div className='menu'>
-                    <div className='menu-btns'>
-                        <button className='menu-btn-register'>REGISTER for Infoshare 2020</button>
-                        <button className='menu-btn-login'>Login</button>
+                <div className='top-header'>
+                    <div className='logo'>
+                        <a href={infoShareWebSiteAdress} className='infoshare-logo' alt='infoShare' />
+                        <a href={gdanskWebSiteAdress} className='gdansk-logo' alt='Gdansk city' />
                     </div>
-                    <a className='menu-hamburger-icon' onClick={() => toggleMenu()} >
-                    </a>
+                    <div className='menu'>
+                        <div className='menu-btns'>
+                            <button className='menu-btn-register'>REGISTER for Infoshare 2020</button>
+                            <button className='menu-btn-login'>Login</button>
+                        </div>
+                        <a className='menu-hamburger-icon' onClick={() => toggleMenu()} >
+                        </a>
+                    </div>
                 </div>
-            </div>
-            <div className='menu-dropdown' style={menu ? { display: 'flex' } : { display: 'none' }}>
-                <a href="#news">News</a>
-                <a href="#contact">Contact</a>
-                <a href="#about">About</a>
-            </div>
-            <div className='bottom-nav'>
-                <ul>
-                    <li>
-                        <a>infoShare 2020</a>
-                        <ul>
-                            <li>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                            </li>
+                <div className='menu-normal' style={menu?{display:'flex'}:{display:'none'}} >
+
+                    <li className='dropdown'>
+                        <a className='dropbtn' alt='dupa' href='https://dupa.pl' class='click-effect'>Infoshare 2020</a>
+                        <ul className='dropbtn-content'>
+                            {infoShareLinks}
                         </ul>
                     </li>
-                    <li>
-                        <a>Why attend</a>
-                        <ul>
-                            <li>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                            </li>
+
+                    <li className='dropdown'>
+                        <a className='dropbtn' alt='dupa' href='https://dupa.pl' class='click-effect'>Poeple</a>
+                        <ul className='dropbtn-content'>
+                            {whyAttendLinks}
                         </ul>
                     </li>
-                    <li>
-                        <a>Outline agenda</a>
-                        <ul>
-                            <li>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                            </li>
+                    <li className='dropdown'>
+                        <a className='dropbtn' alt='dupa' href='https://dupa.pl' class='click-effect'>About</a>
+                        <ul className='dropbtn-content'>
+                            {infoShareLinks}
                         </ul>
                     </li>
-                    <li>
-                        <a>Speakers</a>
-                        <ul>
-                            <li>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                            </li>
+
+                    <li className='dropdown'>
+                        <a className='dropbtn' alt='dupa' href='https://dupa.pl' class='click-effect'>Our Future</a>
+                        <ul className='dropbtn-content'>
+                            {whyAttendLinks}
                         </ul>
                     </li>
-                    <li>
-                        <a>Become a partner</a>
-                        <ul>
-                            <li>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                            </li>
+                    <li className='dropdown'>
+                        <a className='dropbtn' alt='dupa' href='https://dupa.pl' class='click-effect'>Discor FAQ</a>
+                        <ul className='dropbtn-content'>
+                            {infoShareLinks}
                         </ul>
                     </li>
-                    <li>
-                        <a>Startups</a>
-                        <ul>
-                            <li>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                            </li>
+
+                    <li className='dropdown'>
+                        <a className='dropbtn' alt='dupa' href='https://dupa.pl' class='click-effect'>66666</a>
+                        <ul className='dropbtn-content'>
+                            {whyAttendLinks}
                         </ul>
                     </li>
-                    <li>
-                        <a>Our projects</a>
-                        <ul>
-                            <li>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                            </li>
+                    <li className='dropdown'>
+                        <a className='dropbtn' alt='dupa' href='https://dupa.pl' class='click-effect'>Logi Baby</a>
+                        <ul className='dropbtn-content'>
+                            {infoShareLinks}
                         </ul>
                     </li>
-                    <li>
-                        <a>About</a>
-                        <ul>
-                            <li>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                                <a>Stages</a>
-                            </li>
+
+                    <li className='dropdown'>
+                        <a className='dropbtn' alt='dupa' href='https://dupa.pl' class='click-effect'>OK DUDE</a>
+                        <ul className='dropbtn-content'>
+                            {whyAttendLinks}
                         </ul>
                     </li>
-                </ul>
-            </div>
+
+                </div>
             </div>
         </div>
     )
--- src/components/Main/MainHero/MainHero.js
+++ src/components/Main/MainHero/MainHero.js
@@ -9,16 +9,16 @@ export const MainHero = () => {
         <div className='hero'>
             <div className='hero-video'>
                 <video autoPlay className='video' loop='loop' muted preload='auto'>
-                    <source src={infoShareMovie} type='video/mp4' />
+                    <source src='https://www.ok.pl' type='video/mp4' />
                 </video>
             </div>
             <div className='hero-text'>
                 <img src={infoShareWhiteLogo} className='text-logo'/>
                 <div className='text-info'>
-                    <h1 className='info-info-heading'>the biggest tech conference in cee</h1>
-                    <h1 className='info-info-heading'>Gdańsk, AMberexpo, June 19 - 21, 2021</h1>
-                    <h2 className='info-info heading secondary'>Join a truly innovative society and get inspired by the world of technology</h2>
-                    <button className='info-btn-ticket'>Grab your Infoshare ticket</button>
+                    <h1 className='text-info-heading bold'>the biggest tech conference in cee</h1>
+                    <h2 className='text-info-heading'>Gdańsk, AMberexpo, June 19 - 21, 2021</h2>
+                    <h3 className='text-info-heading secondary'>Join a truly innovative society <br/> and get inspired by the world of technology</h3>
+                    <button className='text-btn-ticket'><b>Grab your Infoshare ticket</b></button>
                 </div>
             </div>
         </div>
